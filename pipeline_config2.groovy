d4b3d34c17533e80ce0394843a11fe85ad2f5135
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-09-12 13:02:28 +0530

    Create pipeline_config2.groovy

---
+++ pipeline_config2.groovy
@@ -0,0 +1,25 @@
+@Library('my-shared-library') _
+
+def config = readYaml file: 'config.yaml'
+
+// Access libraries
+def libraries = config.libraries
+libraries.each { lib ->
+    // Load libraries as required
+    echo "Library: ${lib}"
+}
+
+// Check allow_scm_jenkinsfile
+if (config.allow_scm_jenkinsfile) {
+    echo "SCM Jenkinsfile is allowed"
+}
+
+// Access application environments
+def envs = config.application_environments
+envs.each { envName, envConfig ->
+    echo "Environment: ${envName}"
+    echo "IP Addresses: ${envConfig.ip_addresses.join(', ')}"
+    if (envConfig.long_name) {
+        echo "Long Name: ${envConfig.long_name}"
+    }
+}
