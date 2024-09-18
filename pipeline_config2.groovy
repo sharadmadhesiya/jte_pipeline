266aa97b07dd276a2ea5f1a17bbfc45a19db7a6a
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-09-18 19:12:53 +0530

    Update pipeline_config2.groovy

--- pipeline_config2.groovy
+++ pipeline_config2.groovy
@@ -1,38 +1,17 @@
-@Grab(group='org.yaml', module='snakeyaml', version='1.30')
-import org.yaml.snakeyaml.Yaml
-import java.nio.file.Files
-import java.nio.file.Paths
+libraries:
+  - maven
+  - sonarqube
+  - ansible
 
-// Load YAML file
-def loadYaml(String filePath) {
-    def yaml = new Yaml()
-    def inputStream = Files.newInputStream(Paths.get(filePath))
-    return yaml.load(inputStream)
-}
+allow_scm_jenkinsfile: true
 
-// Path to your YAML file
-def filePath = 'config.yaml'
-
-// Read the YAML configuration
-def config = loadYaml(filePath)
-
-// Access and use the configuration
-def libraries = config.libraries ?: []
-libraries.each { lib ->
-    println "Library: ${lib}"
-}
-
-if (config.allow_scm_jenkinsfile) {
-    println "SCM Jenkinsfile is allowed"
-}
-
-def envs = config.application_environments ?: [:]
-envs.each { envName, envConfig ->
-    println "Environment: ${envName}"
-    if (envConfig.ip_addresses) {
-        println "IP Addresses: ${envConfig.ip_addresses.join(', ')}"
-    }
-    if (envConfig.long_name) {
-        println "Long Name: ${envConfig.long_name}"
-    }
-}
+application_environments:
+  dev:
+    ip_addresses:
+      - 192.168.1.1
+      - 192.168.1.2
+    long_name: Development Environment
+  prod:
+    ip_addresses:
+      - 10.0.0.1
+    long_name: Production Environment
