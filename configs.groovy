9ec5b79ff6bcdffcd77c74cf1ae6e3b7216af467
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-09-20 16:00:32 +0530

    Create configs.groovy

---
+++ configs.groovy
@@ -0,0 +1,17 @@
+libraries:
+  - maven
+  - sonarqube
+  - ansible
+
+allow_scm_jenkinsfile: true
+
+application_environments:
+  dev:
+    ip_addresses:
+      - 192.168.1.1
+      - 192.168.1.2
+    long_name: Development Environment
+  prod:
+    ip_addresses:
+      - 10.0.0.1
+    long_name: Production Environment
