5b56fc9a7a900e2f48d5188984718f556ac8ede7
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-30 10:50:06 +0530

    removed syntax

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -10,7 +10,7 @@ dev {
     BRANCH_NAME = 'main'
     REPO_URL='sharadmadhesiya/jte_pipeline.git'
     GIT_HOST='github.com'
-    PATH: 'HelloWorld.xcodeproj', // Update with your project path
+    PATH:'HelloWorld.xcodeproj', // Update with your project path
     CODE_SIGN_IDENTITY: 'Apple Distribution: Johnson and Johnson Consumer Services EAME Limited (D6CR985G9Q)',
     PROFILE_NAME: 'match AppStore com.kenvue.katalyst.demo', // Ensure this profile matches your provisioning profile
     SCHEME: 'HelloWorld'
