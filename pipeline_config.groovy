838cab3220257581b7e1630707d71f1a1b4a8913
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-30 10:51:33 +0530

    removed syntax error

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -10,9 +10,9 @@ dev {
     BRANCH_NAME = 'main'
     REPO_URL='sharadmadhesiya/jte_pipeline.git'
     GIT_HOST='github.com'
-    PATH:'HelloWorld.xcodeproj', // Update with your project path
-    CODE_SIGN_IDENTITY: 'Apple Distribution: Johnson and Johnson Consumer Services EAME Limited (D6CR985G9Q)',
-    PROFILE_NAME: 'match AppStore com.kenvue.katalyst.demo', // Ensure this profile matches your provisioning profile
+    PATH:'HelloWorld.xcodeproj' // Update with your project path
+    CODE_SIGN_IDENTITY: 'Apple Distribution: Johnson and Johnson Consumer Services EAME Limited (D6CR985G9Q)'
+    PROFILE_NAME: 'match AppStore com.kenvue.katalyst.demo' // Ensure this profile matches your provisioning profile
     SCHEME: 'HelloWorld'
 
 }
